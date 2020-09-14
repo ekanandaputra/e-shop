88282c497b8798884a225d375dc74454af4c825a
ekanandaputra <52701387+ekanandaputra@users.noreply.github.com>
2020-09-14 12:27:15 +0700

    Update adminOrderController.php

--- app/Http/Controllers/adminOrderController.php
+++ app/Http/Controllers/adminOrderController.php
@@ -8,17 +8,20 @@ use App\DetailOrder;
 
 class adminOrderController extends Controller
 {
+    // Membuat Controller ini hanya dapat diaksis oleh admin
     public function __construct()
     {
         $this->middleware('auth:admin');
     }
     
+    // Menampilkan list order
     public function showOrder()
     {
         $order = Order::all();
     	return view('admin/order/list_order', ['order' => $order]);
     }
 
+    // Menampilkan detail order
     public function orderDetail($order_id)
     {
         $order = Order::findOrFail($order_id);
@@ -29,6 +32,7 @@ class adminOrderController extends Controller
         ]);
     }
 
+    // mengganti status order menjadi delivered
     public function changeStatus(Request $request, $order_id)
     {
         Order::where('order_id',$order_id)->update([
