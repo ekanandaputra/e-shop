d72d216319fd87b763e41f6c7fe3a3bbdeedef06
ekanandaputra <52701387+ekanandaputra@users.noreply.github.com>
2020-09-14 12:28:31 +0700

    Update adminProductController.php

--- app/Http/Controllers/adminProductController.php
+++ app/Http/Controllers/adminProductController.php
@@ -8,28 +8,33 @@ use App\Product;
 
 class adminProductController extends Controller
 {
+    // Membuat Controller ini hanya dapat diaksis oleh admin
     public function __construct()
     {
         $this->middleware('auth:admin');
     }
     
+    // Menampilkan list produk
     public function showProduct()
     {
         $product = Product::all();
     	return view('admin/product/list_product', ['product' => $product]);
     }
 
+    // Menampilkan form tambah produk
     public function showForm()
     {
         return view('admin/product/add_product');
     }
 
+    // Menyimpan produk ke database
     public function saveProduct(Request $request)
     {
         Product::create($request->all());
         return redirect('/admin/add/product')->with('success', 'Add Product Successfully');
     }
 
+    // menampilkan form edit produk
     public function showEditForm($product_id)
     {
         $product = Product::findOrFail($product_id);
@@ -38,6 +43,7 @@ class adminProductController extends Controller
         ]);
     }
 
+    // Update produk
     public function edit(Request $request, $product_id)
     {
         $product = Product::findOrFail($product_id);
@@ -46,6 +52,7 @@ class adminProductController extends Controller
         return redirect('admin/edit/product/'.$product_id)->with('success', 'Update Product Successfully');
     }
 
+    // Hapus produk
     public function delete($product_id)
     {
         $product = Product::findOrFail($product_id);
